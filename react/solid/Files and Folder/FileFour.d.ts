9a62d20f5c417d18d748ce35390f22924833a963
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-21 23:46:17 +0330

    FileAdd01 Outline

--- react/solid/Files and Folder/FileFour.d.ts
+++ react/solid/Files and Folder/FileFour.d.ts
@@ -1,3 +1,3 @@
 import * as React from 'react';
-declare const HomeOne: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
-export default HomeOne;
+declare const FileFour: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
+export default FileFour;
