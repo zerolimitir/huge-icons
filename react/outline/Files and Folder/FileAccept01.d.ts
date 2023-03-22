a282edf8226072ab7156639300a834a5c2200432
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-22 15:31:32 +0430

    FileUpload Outline

--- react/outline/Files and Folder/FileAccept01.d.ts
+++ react/outline/Files and Folder/FileAccept01.d.ts
@@ -1,3 +1,3 @@
 import * as React from 'react';
-declare const FileAccept01: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
-export default FileAccept01;
+declare const FileUpload: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
+export default FileUpload;
