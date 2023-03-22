b5d7e07cdad89c004b6b21204a65f786c2613ee5
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-22 13:38:52 +0430

    File02 Outline

---
+++ react/outline/Files and Folder/File02.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare const File02: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
+export default File02;
