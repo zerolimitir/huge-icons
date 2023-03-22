0bf366ca00d202bcd49adc6ba1fb422073f1d601
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-22 14:42:58 +0430

    Update File04

--- react/outline/Files and Folder/File04.js
+++ react/outline/Files and Folder/File04.js
@@ -11,6 +11,8 @@ function File04({ size, title, titleId, ...props }, svgRef) {
 				viewBox: "0 0 24 24",
 				fill: "none",
 				stroke: "currentColor",
+				"stroke-width": "1.5",
+				"stroke-linejoin": "round",
 				"aria-hidden": "true",
 				ref: svgRef,
 				"aria-labelledby": titleId,
@@ -20,8 +22,6 @@ function File04({ size, title, titleId, ...props }, svgRef) {
 		title ? /*#__PURE__*/ React.createElement("title", { id: titleId }, title) : null,
 		/*#__PURE__*/ React.createElement("path", {
 			d: "M13 22L13 18C13 15.7909 14.7909 14 17 14L21 14M3 18L3 6C3 3.79086 4.79086 2 7 2L17 2C19.2091 2 21 3.79086 21 6V12.3431C21 13.404 20.5786 14.4214 19.8284 15.1716L14.1716 20.8284C13.4214 21.5786 12.404 22 11.3431 22H7C4.79086 22 3 20.2091 3 18Z",
-			"stroke-width": "1.5",
-			"stroke-linejoin": "round",
 		}),
 	);
 }
