2c211b11ed20a60c5e5c00b9f2ae0819f8483e62
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2023-03-22 17:41:54 +0430

    addcircle icon

---
+++ react/outline/AddCircle.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+function AddCircle({ size, title, titleId, ...props }, svgRef) {
+	return /*#__PURE__*/ React.createElement(
+		"svg",
+		Object.assign(
+			{
+				width: size || "1rem",
+				height: size || "1rem",
+				xmlns: "http://www.w3.org/2000/svg",
+				viewBox: "0 0 24 24",
+				fill: "currentColor",
+				"aria-hidden": "true",
+				ref: svgRef,
+				"aria-labelledby": titleId,
+			},
+			props,
+		),
+		title
+			? /*#__PURE__*/ React.createElement(
+					"title",
+					{
+						id: titleId,
+					},
+					title,
+			  )
+			: null,
+		/*#__PURE__*/ React.createElement("path", {
+			d:"M12 8V16M16 12H8M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z",
+			stroke:"#28303F",
+			 strokeWidth:"1.5",
+			 strokeLinecap:"round",
+			strokeLinejoin:"round"
+			}),
+	);
+}
+
+
+export default  React.forwardRef(AddCircle);
