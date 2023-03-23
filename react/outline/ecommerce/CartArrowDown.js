f59b7ba407fc490807987e009176420e3b01feed
AmirMahdi Shad <amirshadnew@gmail.com>
2023-03-23 11:11:05 +0430

    CartArrowDown

---
+++ react/outline/ecommerce/CartArrowDown.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+
+function CartArrowDown({ size, title, titleId, ...props }, svgRef) {
+  return /*#__PURE__*/ React.createElement(
+    "svg",
+    Object.assign(
+      {
+        width: size || "1rem",
+        height: size || "1rem",
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 24 24",
+        fill: "none",
+        stroke: "currentColor",
+        strokeWidth: "1.5",
+        "aria-hidden": "true",
+        ref: svgRef,
+        "aria-labelledby": titleId,
+      },
+      props
+    ),
+    title
+      ? /*#__PURE__*/ React.createElement(
+          "title",
+          {
+            id: titleId,
+          },
+          title
+        )
+      : null,
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M6 4H18C20.2091 4 22 5.79086 22 8V13C22 15.2091 20.2091 17 18 17H10C7.79086 17 6 15.2091 6 13V4ZM6 4C6 2.89543 5.10457 2 4 2H2",
+      strokeLinejoin: "round",
+      strokeLineCap: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M6 5L5.63927 2.8356C5.5589 2.35341 5.14171 2 4.65287 2H2",
+      strokeLineCap: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M11 20.5C11 21.3284 10.3284 22 9.5 22C8.67157 22 8 21.3284 8 20.5C8 19.6716 8.67157 19 9.5 19C10.3284 19 11 19.6716 11 20.5Z",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M20 20.5C20 21.3284 19.3284 22 18.5 22C17.6716 22 17 21.3284 17 20.5C17 19.6716 17.6716 19 18.5 19C19.3284 19 20 19.6716 20 20.5Z",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M16 12L14.7071 13.2929C14.3166 13.6834 13.6834 13.6834 13.2929 13.2929L12 12M14 8V13",
+      strokeLineCap: "round",
+    })
+  );
+}
+
+export default React.forwardRef(CartArrowDown);
