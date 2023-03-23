9fe9ff2ca169385ec5a8ffdb7d8b5f06407dda00
AmirMahdi Shad <amirshadnew@gmail.com>
2023-03-23 12:36:59 +0430

    ShoppingBagRemove

---
+++ react/outline/ecommerce/ShoppingBagRemove.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare const ShoppingBagRemove: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
+export default ShoppingBagRemove;
