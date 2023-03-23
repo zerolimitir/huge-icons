442394c8cf4d7e201dba165eefdc013763ff22b1
AmirMahdi Shad <amirshadnew@gmail.com>
2023-03-23 12:39:56 +0430

    ShoppingBagCheck

---
+++ react/outline/ecommerce/ShoppingBagCheck.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare const ShoppingBagCheck: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
+export default ShoppingBagCheck;
