48a1397b2fb794096328e9dd57b972d11be72a4a
AmirMahdi Shad <amirshadnew@gmail.com>
2023-03-24 13:59:22 +0430

    ShoppingBasket02

---
+++ react/outline/ecommerce/ShoppingBasket02.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+
+function ShoppingBasket02({ size, title, titleId, ...props }, svgRef) {
+  return /*#__PURE__*/ React.createElement(
+    "svg",
+    Object.assign(
+      {
+        width: size || "1rem",
+        height: size || "1rem",
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 24 24",
+        fill: "none",
+        stroke: "currentColor",
+        strokeWidth: "1.5",
+        "aria-hidden": "true",
+        ref: svgRef,
+        "aria-labelledby": titleId,
+      },
+      props
+    ),
+    title
+      ? /*#__PURE__*/ React.createElement(
+          "title",
+          {
+            id: titleId,
+          },
+          title
+        )
+      : null,
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M2.58683 10H21.4132M18.0351 6L5.96486 6C3.45403 6 1.57594 8.32624 2.08312 10.808L3.71804 18.808C4.09787 20.6666 5.71942 22 7.59978 22H16.4002C18.2806 22 19.9021 20.6666 20.282 18.808L21.9169 10.808C22.4241 8.32624 20.546 6 18.0351 6Z",
+      strokeLinejoin: "round",
+      strokeLineCap: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M9 2L6 6",
+      strokeLinejoin: "round",
+      strokeLineCap: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M15 2L18 6",
+      strokeLinejoin: "round",
+      strokeLineCap: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M9 14L9 18",
+      strokeLinejoin: "round",
+      strokeLineCap: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M15 14L15 18",
+      strokeLinejoin: "round",
+      strokeLineCap: "round",
+    })
+  );
+}
+export default React.forwardRef(ShoppingBasket02);
