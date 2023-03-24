30869004958f702bcde3fbe1b6f4933f8b960c79
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2023-03-24 14:54:55 +0430

    eror for camel case now its ok

--- react/outline/Interface/ClockRectangleDouble.js
+++ react/outline/Interface/ClockRectangleDouble.js
@@ -11,7 +11,7 @@ function ClockRectangleDouble({ size, title, titleId, ...props }, svgRef) {
 				viewBox: "0 0 24 24",
 				fill: "none",
 				stroke: "currentColor",
-				"stroke-width": "1.5",
+				"strokeWidth":"1.5",
 				"stroke-linecap": "round",
 				"stroke-linejoin": "round",
 				"aria-hidden": "true",
