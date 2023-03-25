54116f688aaa5e6708c8f8d54d83c3707936e110
AmirMahdi Shad <amirshadnew@gmail.com>
2023-03-25 18:19:15 +0430

    Wallet

---
+++ react/outline/ecommerce/Wallet.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+function Wallet({ size, title, titleId, ...props }, svgRef) {
+  return /*#__PURE__*/ React.createElement(
+    "svg",
+    Object.assign(
+      {
+        width: size || "1rem",
+        height: size || "1rem",
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 24 24",
+        fill: "none",
+        stroke: "currentColor",
+        strokeWidth: "1.5",
+        "aria-hidden": "true",
+        ref: svgRef,
+        "aria-labelledby": titleId,
+      },
+      props
+    ),
+    title
+      ? /*#__PURE__*/ React.createElement(
+          "title",
+          {
+            id: titleId,
+          },
+          title
+        )
+      : null,
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M2 7C2 4.79086 3.79086 3 6 3H18C20.2091 3 22 4.79086 22 7V17C22 19.2091 20.2091 21 18 21H6C3.79086 21 2 19.2091 2 17V7Z",
+      strokeLinejoin: "round",
+    }),
+    /*#__PURE__*/ React.createElement("path", {
+      d: "M22 9L22 15H18C16.3431 15 15 13.6569 15 12C15 10.3431 16.3431 9 18 9L22 9Z",
+      strokeLinejoin: "round",
+    })
+  );
+}
+export default React.forwardRef(Wallet);
