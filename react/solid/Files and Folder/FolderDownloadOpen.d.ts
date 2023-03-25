56603127f3520db65402a2f87917ad7549355f96
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-26 00:21:01 +0430

    added solif icon

--- react/solid/Files and Folder/FolderDownloadOpen.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-declare const FolderDownloadOpen: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
-export default FolderDownloadOpen;
--- react/solid/Files and Folder/FolderSearch.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-declare const FolderSearch: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & { size?: string, title?: string, titleId?: string }>;
-export default FolderSearch;
